b21e2cedd74ace9fe4c1d50146d46fe298a5c18c
unknown <sagan.alona201408@gmail.com>
2020-08-27 17:26:03 +0300

    new folder

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log('hello hexlet!!');
-console.log(_.capitalize('hello'));
-console.log(_.last(['one', 'two']));
+console.log("hello hexlet!!");
+console.log(_.capitalize("hello"));
+console.log(_.last(["one", "two"]));
+
+import half from "./src/half.js";
+
+export default half;
---
+++ src/half.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+import _ from "lodash";
+
+/**
+ * @param {Number} num
+ */
+
+export default (num) => {
+  const result = num / 2;
+  _.noop();
+  return result;
+};
